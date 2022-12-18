be91a4d97ab651bb19128c5fa0c76b15e5644747
Automotive Lily <33378913+AutomotiveLily@users.noreply.github.com>
2022-12-18 18:00:10 +0200

    Add update URL

--- Motonet- Show product description.user.js
+++ Motonet- Show product description.user.js
@@ -1,15 +1,19 @@
 // ==UserScript==
-// @name         Motonet: Show product description
-// @name:fi      Motonet: Näytä tuotekuvaus
-// @namespace    https://github.com/AutomotiveLily/userscript
-// @version      0.1
-// @description  Change to product description tab after loading the page.
-// @description:fi Vaihda tuotekuvaus-välilehteen sivun latauduttua
-// @author       AutomotiveLily
-// @match        https://www.motonet.fi/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=motonet.fi
-// @grant        none
+// @name            Motonet: Show product description
+// @name:fi         Motonet: Näytä tuotekuvaus
+// @namespace       https://github.com/AutomotiveLily/userscript
+// @version         0.2
+// @downloadURL     https://github.com/AutomotiveLily/userscript/raw/main/Motonet-%20Show%20product%20description.user.js
+// @updateURL       https://github.com/AutomotiveLily/userscript/raw/main/Motonet-%20Show%20product%20description.user.js
+// @description     Change to product description tab after loading the page.
+// @description:fi  Vaihda tuotekuvaus-välilehteen sivun latauduttua
+// @author          AutomotiveLily
+// @match           https://www.motonet.fi/*
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=motonet.fi
+// @grant           none
 // ==/UserScript==
+
+// Modified from this script: https://stackoverflow.com/a/19343058
 
 // Wait for the page to load.
 // Odota että sivu latautuu.
